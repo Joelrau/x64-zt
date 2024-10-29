7a5df4b922ccb0790467ef44475c89e112ba01a9
quaK <Joel.rautavuori@gmail.com>
2024-10-29 18:42:29 +0200

    gfximage dump update for h1

--- src/zonetool/zonetool/h1/assets/gfximage.cpp
+++ src/zonetool/zonetool/h1/assets/gfximage.cpp
@@ -486,10 +486,103 @@ namespace zonetool::h1
 		}
 	}
 
+	void dump_streamed_image(GfxImage* image, bool is_self = false, bool dump_dds = false)
+	{
+		for (auto i = 0u; i < 4; i++)
+		{
+			const auto stream_file = &stream_files[*stream_file_index + i];
+
+			std::string filename = utils::string::va("imagefile%d.pak", stream_file->fileIndex);
+			if (is_self)
+			{
+				filename = utils::string::va("%s.pak", filesystem::get_fastfile().data());
+			}
+			const auto folder = filesystem::get_zone_path(filename);
+
+			const auto imagefile_path = utils::string::va("%s%s", folder.data(), filename.data());
+			std::ifstream imagefile;
+			imagefile.open(imagefile_path, std::ios::binary);
+
+			if (!imagefile.is_open() || stream_file->offset == 0 || stream_file->offsetEnd == 0)
+			{
+				continue;
+			}
+
+			std::string buffer;
+			const auto size = stream_file->offsetEnd - stream_file->offset;
+			buffer.resize(size);
+			imagefile.seekg(stream_file->offset);
+			imagefile.read(buffer.data(), size);
+
+			try
+			{
+				auto pixel_data = compression::lz4::decompress_lz4_block(buffer);
+				const auto name = clean_name(image->name);
+
+				{
+					std::string parent_path = filesystem::get_dump_path() + "streamed_images\\";
+					std::string raw_path = utils::string::va("%s%s_stream%i.pixels", parent_path.data(), name.data(), i);
+					utils::io::write_file(raw_path, pixel_data, false);
+				}
+
+				if (dump_dds)
+				{
+					DirectX::Image img = {};
+
+					img.width = image->streams[i].width;
+					img.height = image->streams[i].height;
+					img.pixels = reinterpret_cast<uint8_t*>(pixel_data.data());
+					img.format = DXGI_FORMAT(image->imageFormat);
+
+					size_t row_pitch{};
+					size_t slice_pitch{};
+
+					DirectX::ComputePitch(img.format, img.width, img.height, row_pitch, slice_pitch);
+
+					img.rowPitch = row_pitch;
+					img.slicePitch = slice_pitch;
+
+					const auto parent_path = filesystem::get_dump_path() + "streamed_images\\";
+					const std::string spath = utils::string::va("%s\\%s_stream%i.dds", parent_path.data(),
+						name.data(), i);
+					const std::wstring wpath(spath.begin(), spath.end());
+					if (!std::filesystem::exists(parent_path))
+					{
+						std::filesystem::create_directories(parent_path);
+					}
+
+					auto result = DirectX::SaveToDDSFile(img, DirectX::DDS_FLAGS_NONE, wpath.data());
+					if (FAILED(result))
+					{
+						ZONETOOL_WARNING("Failed to dump image \"%s.dds\"", image->name);
+					}
+				}
+			}
+			catch (...)
+			{
+				ZONETOOL_ERROR("Failed to dump streamed image \"%s\"", image->name);
+			}
+		}
+
+		{
+			const auto path = "streamed_images\\"s + clean_name(image->name) + ".h1Image"s;
+			assetmanager::dumper write;
+			if (!write.open(path))
+			{
+				return;
+			}
+
+			write.dump_single(image);
+			write.dump_string(image->name);
+			write.close();
+		}
+	}
+
 	void dump_image_dds(GfxImage* image)
 	{
 		if (image->streamed)
 		{
+			dump_streamed_image(image, stream_files[*stream_file_index].fileIndex == 96, true);
 			return;
 		}
 
@@ -569,63 +662,6 @@ namespace zonetool::h1
 		if (FAILED(result))
 		{
 			ZONETOOL_WARNING("Failed to dump image \"%s\"", spath.data());
-		}
-	}
-
-	void dump_streamed_image(GfxImage* image, bool is_self = false)
-	{
-		for (auto i = 0u; i < 4; i++)
-		{
-			const auto stream_file = &stream_files[*stream_file_index + i];
-
-			std::string filename = utils::string::va("imagefile%d.pak", stream_file->fileIndex);
-			if (is_self)
-			{
-				filename = utils::string::va("%s.pak", filesystem::get_fastfile().data());
-			}
-			const auto folder = filesystem::get_zone_path(filename);
-
-			const auto imagefile_path = utils::string::va("%s%s", folder.data(), filename.data());
-			std::ifstream imagefile;
-			imagefile.open(imagefile_path, std::ios::binary);
-
-			if (!imagefile.is_open() || stream_file->offset == 0 || stream_file->offsetEnd == 0)
-			{
-				continue;
-			}
-
-			std::string buffer;
-			const auto size = stream_file->offsetEnd - stream_file->offset;
-			buffer.resize(size);
-			imagefile.seekg(stream_file->offset);
-			imagefile.read(buffer.data(), size);
-
-			try
-			{
-				auto pixel_data = compression::lz4::decompress_lz4_block(buffer);
-				const auto name = clean_name(image->name);
-
-				std::string parent_path = filesystem::get_dump_path() + "streamed_images\\";
-				std::string raw_path = utils::string::va("%s%s_stream%i.pixels", parent_path.data(), name.data(), i);
-				utils::io::write_file(raw_path, pixel_data, false);
-			}
-			catch (...)
-			{
-				ZONETOOL_ERROR("Failed to dump streamed image \"%s\"", image->name);
-			}
-		}
-
-		{
-			const auto path = "streamed_images\\"s + clean_name(image->name) + ".h1Image"s;
-			assetmanager::dumper write;
-			if (!write.open(path))
-			{
-				return;
-			}
-
-			write.dump_single(image);
-			write.dump_string(image->name);
-			write.close();
 		}
 	}
 
