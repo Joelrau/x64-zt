d8337abf4056b2254ed2729a8dfa613c7deffd9a
quaK <Joel.rautavuori@gmail.com>
2025-10-02 17:20:41 +0300

    Add DirectX image support for CLUT assets
    Introduces DirectXTex-based image loading and dumping for CLUT assets, supporting DDS, TGA, and PNG formats. The clut::parse and clut::dump methods now use DirectX routines, enabling more flexible asset handling and output.

--- src/zonetool/zonetool/h1/assets/clut.cpp
+++ src/zonetool/zonetool/h1/assets/clut.cpp
@@ -1,10 +1,162 @@
 #include "std_include.hpp"
 #include "clut.hpp"
 
+#pragma warning( push )
+#pragma warning( disable : 4459 )
+#include <DirectXTex.h>
+#pragma warning( pop )
+
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+
 namespace zonetool::h1
 {
+	namespace directx
+	{
+		bool load_image(const std::string& name, DirectX::ScratchImage* image)
+		{
+			std::string c_name = name;
+			c_name = utils::string::va("clut\\%s", c_name.data());
+
+			// append suffixes
+			if (filesystem::file(c_name + ".dds").exists())
+				c_name.append(".dds"); // DDS Found
+			else if (filesystem::file(c_name + ".tga").exists())
+				c_name.append(".tga"); // TGA Found
+			else if (filesystem::file(c_name + ".png").exists())
+				c_name.append(".png"); // PNG Found
+			else
+				return false; // No image found
+
+			std::string path = filesystem::get_file_path(c_name) + c_name;
+
+			std::wstring wname = utils::string::convert(path);
+
+			HRESULT hr = E_FAIL;
+			if (c_name.ends_with(".dds"))
+				hr = LoadFromDDSFile(wname.data(), DirectX::DDS_FLAGS_NONE, nullptr, *image);
+			else if (c_name.ends_with(".tga"))
+				hr = LoadFromTGAFile(wname.data(), nullptr, *image);
+			else if (c_name.ends_with(".png"))
+				hr = LoadFromWICFile(wname.data(), DirectX::WIC_FLAGS_NONE, nullptr, *image);
+
+			return SUCCEEDED(hr);
+		}
+
+		Clut* parse(const std::string& name, zone_memory* mem)
+		{
+			DirectX::ScratchImage image;
+
+			if (load_image(name, &image))
+			{
+				ZONETOOL_INFO("Parsing clut image \"%s\"", name.data());
+
+				auto* pixels = image.GetPixels();
+				auto pixels_size = image.GetPixelsSize();
+				const auto& metadata = image.GetMetadata();
+
+				assert(metadata.format == DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM);
+
+				auto* clut = mem->allocate<Clut>();
+				clut->width = static_cast<unsigned short>(metadata.width);
+				clut->height = static_cast<unsigned short>(metadata.height);
+				clut->depth = static_cast<unsigned short>(metadata.depth);
+				assert(metadata.arraySize == 1);
+				assert(metadata.mipLevels == 1);
+				clut->pixels = mem->allocate<unsigned char>(pixels_size);
+				memcpy(clut->pixels, pixels, pixels_size);
+				clut->name = mem->duplicate_string(name);
+
+				return clut;
+			}
+
+			return nullptr;
+		}
+
+		void dump_clut_image_dds(Clut* asset)
+		{
+			const auto width = asset->width * asset->depth;
+			const auto height = asset->height;
+			const auto depth = asset->depth;
+			unsigned char* pixels = reinterpret_cast<unsigned char*>(asset->pixels);
+			const auto dataLen = 4 * width * height * depth;
+
+			auto* data = pixels;
+			std::size_t data_used = 0;
+
+			const auto sides = 1;
+
+			std::vector<DirectX::Image> images{};
+			for (int idx = 0; idx < 1; idx++)
+			{
+				for (int s = 0; s < sides; s++)
+				{
+					for (unsigned int d = 0; d < depth; d++)
+					{
+						for (int i = 0; i < 1; i++)
+						{
+							DirectX::Image img{};
+							img.pixels = data;
+
+							img.width = max(1, width >> i);
+							img.height = max(1, height >> i);
+							img.format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM;
+
+							size_t rowPitch = 0;
+							size_t slicePitch = 0;
+							DirectX::ComputePitch(img.format, img.width, img.height, rowPitch, slicePitch);
+
+							img.rowPitch = rowPitch;
+							img.slicePitch = slicePitch;
+
+							images.push_back(img);
+
+							data += slicePitch;
+							data_used += slicePitch;
+						}
+					}
+				}
+			}
+
+			if (data_used != dataLen)
+			{
+				ZONETOOL_WARNING("Failed to dump image \"%s.dds\"", asset->name);
+				return;
+			}
+
+			DirectX::TexMetadata mdata{};
+			mdata.width = width;
+			mdata.height = height;
+			mdata.depth = depth;
+			mdata.arraySize = 1 * sides;
+			mdata.mipLevels = 1;
+			mdata.format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM;
+			mdata.dimension = DirectX::TEX_DIMENSION::TEX_DIMENSION_TEXTURE2D;
+
+			std::string parent_path = filesystem::get_dump_path() + "clut\\";
+			std::string spath = parent_path + asset->name + ".dds";
+			std::wstring wpath(spath.begin(), spath.end());
+
+			if (!std::filesystem::exists(parent_path))
+			{
+				std::filesystem::create_directories(parent_path);
+			}
+
+			auto result = DirectX::SaveToDDSFile(images.data(), images.size(), mdata, DirectX::DDS_FLAGS_NONE, wpath.data());
+			if (FAILED(result))
+			{
+				ZONETOOL_WARNING("Failed to dump image \"%s\"", spath.data());
+			}
+		}
+	}
+
 	Clut* clut::parse(const std::string& name, zone_memory* mem)
 	{
+		if (auto* asset = directx::parse(name, mem))
+		{
+			return asset;
+		}
+
 		assetmanager::reader read(mem);
 
 		const auto path = "clut\\"s + name + ".clut"s;
@@ -79,18 +231,6 @@ namespace zonetool::h1
 
 	void clut::dump(Clut* asset)
 	{
-		const auto path = "clut\\"s + asset->name + ".clut";
-
-		assetmanager::dumper dumper;
-		if (!dumper.open(path))
-		{
-			return;
-		}
-
-		dumper.dump_single(asset);
-		dumper.dump_string(asset->name);
-		dumper.dump_array(asset->pixels, 4 * asset->width * asset->height * asset->depth);
-
-		dumper.close();
+		directx::dump_clut_image_dds(asset);
 	}
 }
