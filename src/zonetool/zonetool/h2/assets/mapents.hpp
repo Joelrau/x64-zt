8d22aeab1cbfb718a4e0d8303c845d6356b44494
alice <58637860+alicealys@users.noreply.github.com>
2025-10-19 21:59:15 +0200

    h2 gfxworld stuff

--- src/zonetool/zonetool/h2/assets/mapents.hpp
+++ src/zonetool/zonetool/h2/assets/mapents.hpp
@@ -28,6 +28,9 @@ namespace zonetool::h2
 		void parse_entity_strings(zone_memory* mem, std::string name, char** entityStrings, int* numEntityChars);
 
 	public:
+		static void add_entity_string(const std::string& line);
+		static void clear_entity_strings();
+
 		MapEnts* parse(std::string name, zone_memory* mem);
 
 		static void convert_ents(MapEnts* ents, zone_memory* mem);
